8d83975f125d641851754ec99f9f85abd7729902
Ajinkya <ajinkya.edu6643@gmail.com>
2026-06-06 14:51:36 +0530

    feat: Initial Spring Boot setup with PostgresSQL configuration

---
+++ src/main/java/com/passbookvault/PassbookVaultApplication.java
@@ -0,0 +1,13 @@
+package com.passbookvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassbookVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassbookVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/passbookvault/PassbookVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passbookvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassbookVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
